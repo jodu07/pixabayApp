b3ec108125f2469c71df334b9304c88c8a20b081
jodu07 <jhonnat.dussan@gmail.com>
2022-04-01 14:32:05 -0500

    components

---
+++ src/app/components/listar-imagen/listar-imagen.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ImagenService } from 'src/app/services/imagen.service';
+
+@Component({
+  selector: 'app-listar-imagen',
+  templateUrl: './listar-imagen.component.html',
+  styleUrls: ['./listar-imagen.component.css']
+})
+export class ListarImagenComponent implements OnInit {
+
+    termino = '';
+    suscription: Subscription;
+
+  constructor(private _imagenService: ImagenService) {
+    this.suscription = this._imagenService.getTerminoBusqueda().subscribe(data => {
+      this.termino = data;
+      this.obtenerImagenes();
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  obtenerImagenes(){
+    this._imagenService.getImagenes(this.termino).subscribe(data =>{
+      console.log(data);
+
+      if(data.hits.length === 0){
+        this._imagenService.setError('Opss.. no encontramos ningun resultado');        
+        return;
+      }
+    })
+  }
+
+}
---
+++ src/app/services/imagen.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagenService {
+  private error$ = new Subject<string>();
+  private terminoBusqueda$ = new Subject<string>();
+
+  constructor(private http: HttpClient) { }
+
+  setError(mensaje: string) {
+    this.error$.next(mensaje);
+  }
+
+  getError(): Observable<string>{
+    return this.error$.asObservable();
+  }
+
+  enviarTerminoBusqueda(termino: string) {
+    this.terminoBusqueda$.next(termino);
+  }
+
+  getTerminoBusqueda(): Observable<string>{
+    return this.terminoBusqueda$.asObservable();
+  }
+
+  getImagenes(termino: string): Observable<any>{
+    const KEY = '22238875-b8c569e03520d7784ddf91fcd';
+    const URL = 'https://pixabay.com/api/?key='+ KEY +'&q'+termino;
+    return this.http.get(URL);
+  }
+}
