adde54508e6e35962cd0a2e28aace68902feae03
jodu07 <jhonnat.dussan@gmail.com>
2022-05-03 19:32:24 -0500

    buscarImagen

--- src/app/components/buscar-imagen/buscar-imagen.component.ts
+++ src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -10,13 +10,24 @@ export class BuscarImagenComponent implements OnInit {
 
 
 
-  
+  nombreImagen!: string;
   constructor(private _imagenService: ImagenService) {
    
 
    }
 
   ngOnInit(): void {
+  }
+
+  buscarImagenes(){
+    //console.log(this.nombreImagen);
+    if(this.nombreImagen === ''){
+      this._imagenService.setError('Agrega un texto de busqueda')
+      return;
+    }
+
+    this._imagenService.enviarTerminoBusqueda(this.nombreImagen); 
+
   }
 
 
